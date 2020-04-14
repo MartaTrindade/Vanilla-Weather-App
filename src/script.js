84fab457aff0c1367c6cbf99b045626b8a7fde3b
MartaTrindade <60898719+MartaTrindade@users.noreply.github.com>
2020-04-14 23:27:07 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ function formatDate(date) {
 }
 
 //👨‍🏫Weather API
-function showWeather(response) {
+function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
   document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
@@ -41,7 +41,7 @@ function showWeather(response) {
 function searchCity(city) {
   let apiKey = "2e83a4b7ba2b243a8588825e9765fe5a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(displayWeather);
 }
 
 function searchSubmit(event) {
@@ -51,17 +51,17 @@ function searchSubmit(event) {
 }
 
 //🌍Geolocation
-function showPosition(position) {
+function searchLocation(position) {
   let apiKey = "2e83a4b7ba2b243a8588825e9765fe5a";
   let lat = (position.coords.latitude);
   let lon = (position.coords.longitude);
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   
-  axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(displayWeather);
 }
 function getCurrentLocation(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 //🌡Celsius-Fartnheit
@@ -91,13 +91,9 @@ dateElement.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchSubmit);
 
-let city = null;
-
 //🌍Geolocation
 let currentLocationButton = document.querySelector("#current-location");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
-navigator.geolocation.getCurrentPosition(showPosition);
 
 //🌡Celsius-Fartnheit
 let celsiusTemperature = null;
@@ -107,3 +103,5 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+searchCity("Porto");
